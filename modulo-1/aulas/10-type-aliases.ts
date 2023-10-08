86393e4029a469df128bdeb4e43b1a2ed997721d
Guilherme Almeida <gui.almeidacrz@gmail.com>
2023-10-08 19:06:14 -0300

    feat: type aliases

---
+++ modulo-1/aulas/10-type-aliases.ts
@@ -0,0 +1,38 @@
+{
+  type MeuTipo = string
+  function foo(x: MeuTipo) {
+    return x
+  }
+
+  type NullableString = string | null
+  function sayHello(name: NullableString) {
+    if(name) console.log(name)
+    return name
+  }
+
+  type Nullable<T> = T | null
+  function sayHello2(name: Nullable<string>) {
+    if(name) console.log(name)
+    return name
+  }
+
+  const objeto: { name: string } = { name: 'Guilherme' }
+  const user: { name: string } = { name: 'ana' }
+}
+
+{
+  type User = {
+    name: string
+    age?: number
+  }
+
+  const objeto: User = { name: 'Guilherme' }
+  const user: User = { name: 'ana' }
+}
+
+{
+  type A = string
+  type B = number
+  type C = undefined
+  type D = A | B | C
+}
