b5c10e43e49d0dec03a73ee1fe40684dd910a964
Guilherme Almeida <gui.almeidacrz@gmail.com>
2023-10-08 18:06:32 -0300

    feat: primitive types

---
+++ modulo-1/aulas/1-tipos-primitivos.ts
@@ -0,0 +1,13 @@
+{
+  const nome: string = 'Guilherme'
+  const idade: number = 25
+  const programmer: boolean = true
+
+  function ola(nome: string): void {
+    console.log(`olá ${nome}`)
+  }
+
+  class Pessoa {
+    nome: string = 'Guilherme'
+  }
+}
---
+++ modulo-1/aulas/2-inferencia.ts
@@ -0,0 +1,13 @@
+{
+  let nome = 'Guilherme'
+  let idade = 25
+  let programmer = true
+
+  function ola(nome: string): void {
+    console.log(`olá ${nome}`)
+  }
+
+  let arr: [1, 'guilherme', true, null]
+
+  const numeros: number[] = [1,2,3,4,5]
+}
